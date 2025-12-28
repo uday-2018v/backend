87f8a13661f24e63527aba72cf63455576ae2fda
= <=>
2025-12-28 17:19:30 +0530

    connect mongodb

---
+++ db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constants.js";
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    console.log(`\n MongoDB connected successfully !! DB host: ${connectionInstance.connection.host}`)
+    ;}
+    catch (error) {
+      console.error("Error connecting to database:", error);
+      process.exit(1);
+    }
+};
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-kdkdkdkd
+import mongoose, { connect } from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "../db/index.js";
+import { DB_NAME } from "./constants.js";
+dotenv.config(
+    {
+        path: "./env"
+    }
+);
+
+
+
+
+
+
+
+
+
+connectDB();
+
+// import express from "express";
+// const app = express();
+// (async () => {
+//     try{ 
+//         mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         app.on("error",(error)=>{
+//             console.log("Error:",error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     }
+//     catch(error){
+//         console.error("Error:", error);
+//         throw error;
+//     }
+// })()
